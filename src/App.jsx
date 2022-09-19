fbc78b38cc1cb51d37acbd0819fa8f9bd9a42cf9
Chamilka Sanduni <80202954+Chamilka-sandu@users.noreply.github.com>
2022-09-19 20:24:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-//import logo from './logo.svg';
-//import './App.css';
+
 import React, { Component } from 'react';
 import SimpleForm from './component/chatbot/SimpleForm';
 import {BrowserRouter as Router, Switch, Route, Routes} from "react-router-dom";
